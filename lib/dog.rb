328e563b6133a721f27faa79a30905b05bf473bf
jasonkwong11 <jasonkwong11@gmail.com>
2016-03-23 22:34:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+def name
+  @name
+end
+
+  def name=(argument)
+      @name = argument
+    end
+
+  def breed
+    @breed
+  end
+
+  def breed=(arg)
+    @breed = arg
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(argu)
+    @name = argu
+  end
+
+  def job
+  @job
+  end
+
+  def job=(arg)
+    @job = arg
+  end
+
+
+end
